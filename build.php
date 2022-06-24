37844a5c336d8e7493ab8bd2221e38ca6971ac62
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-06-24 11:07:46 +0200

    Create build.php boilerplate

---
+++ build.php
@@ -0,0 +1,62 @@
+<?php
+    println("Ensuring composer...");
+    if(!execute("composer require rosell-dk/webp-convert")) {
+        println("Error installing webp-convert!");
+        exit;
+    }
+    println("Composer installed!\n");
+
+    println("Cleaning build directory...");
+    deleteFolder("build");
+
+    println("\nStarting build...");
+    mkdir("build");
+    copyFolder("src/resources/", "build");
+?>
+
+<?php
+    function println($message) {
+        echo $message . "\n";
+    }
+
+    function execute($command) {
+        $returnVal;
+        exec($command, $ignore, $returnVal);
+        return $returnVal == 0;
+    }
+
+    function fixSlashes($string) {
+        return (DIRECTORY_SEPARATOR === '\\') ? str_replace('/', '\\', $string) : str_replace('\\', '/', $string);
+    }
+
+    function copyFolder($folder, $dest) {
+        $it = new RecursiveDirectoryIterator($folder, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($files as $file) {
+            if(!is_dir($file)) {
+                //$file in this case contains src/resources/ which we want to cut out using str_replace
+                $newFile = $dest . DIRECTORY_SEPARATOR . str_replace(fixSlashes($folder), "", fixSlashes($file));
+                makedir($newFile);
+                copy($file, $newFile);
+            }
+        }
+    }
+
+    function makedir($folder) {
+        $dirName = dirname($folder);
+        if(!file_exists($dirName))
+            mkdir($dirName, 0777, true);
+    }
+
+    function deleteFolder($folder) {
+        if(!file_exists($folder)) return;
+
+        $it = new RecursiveDirectoryIterator($folder, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($files as $file) {
+            $path = $file->getRealPath();
+            is_dir($path) ? rmdir($path) : unlink($path);
+        }
+        rmdir($folder);
+    }
+?>
